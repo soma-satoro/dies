20de2dd0b43cb1876fbb032dd933e0ef4258f4f9
Lem Canady <lem.canady@gmail.com>
2024-08-02 19:33:56 -0700

    add stat setting/management via character object,

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -1,10 +1,11 @@
 # typeclasses/characters.py
 from evennia import DefaultCharacter
-from world.wod20th.models import Stat
 
 class Character(DefaultCharacter):
     
     def get_stat(self, stat_name):
+        if not hasattr(self.db, "stats"):
+            return None
         return self.db.stats.get(stat_name, None)
     
     def set_stat(self, stat_name, value):
@@ -13,7 +14,9 @@ class Character(DefaultCharacter):
         self.db.stats[stat_name] = value
     
     def check_stat_value(self, stat_name, value):
+        from world.wod20th.models import Stat  # Import here to avoid circular reference
         stat = Stat.objects.filter(name=stat_name).first()
         if stat and (value in stat.values):
             return True
         return False
+
